4a5b6d08e6401965daf1f4e7c9015bcc9717d017
rajm012 <rajmahimaurya@gmail.com>
2025-12-09 22:24:23 +0530

    Init-Git

---
+++ 2.Ours-Mod/lib/train_util.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import torch
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def save_model(model, optimizer, args, epoch, save_file):
+    """Save model checkpoint"""
+    print('==> Saving...')
+    state = {
+        'args': args,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'epoch': epoch,
+    }
+    torch.save(state, save_file)
+    del state
+    
+    
